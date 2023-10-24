88a4754244429c4ce3d2860a6c457aa8d838a297
Harish1611 <harishnaidub005@gmail.com>
2023-10-24 17:40:51 +0530

    Extended: Added Popup Styles for ErrorPopupModal with overlay

--- src/components/AddUser.jsx
+++ src/components/AddUser.jsx
@@ -16,7 +16,8 @@ const AddUser = (props) => {
       });
       return;
     }
-    if (userAge.trim().length <= 0) {
+
+    if (userAge.trim() <= 0) {
       setError({
         title: "Invalid Age",
         message: "Please Enter Age > 0",
@@ -44,56 +45,58 @@ const AddUser = (props) => {
   };
 
   return (
-    <div className="justify-center ">
-      {error && (
-        <ErrorPopupModel
-          title={error.title}
-          message={error.message}
-          onConfrim={errorHandler}
-        />
-      )}
-      <div className="flex p-4 ">
-        <div className="mt-14 p-8   bg-gradient-to-r from-sky-900 via-sky-600 to-sky-950 border-x-gray-700 	items-center border-2 rounded-xl w-full lg:w-[35%]  shadow-lg mx-auto ">
-          <form onSubmit={userSubmissionHandler} className="">
-            <label
-              htmlFor="username"
-              className="block mt-2 w-full text-md font-bold leading-6 text-black"
-            >
-              User Name
-            </label>
-            <div className="flex rounded-md w-full  bg-white   shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <input
-                type="text"
-                name="userName"
-                value={userName}
-                onChange={userNameHandler}
-                className="block flex-1  border-0 bg-transparent py-1.5 pl-1 text-gray-800 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              />
-            </div>
+    <div>
+      <div className=" ">
+        {error && (
+          <ErrorPopupModel
+            title={error.title}
+            message={error.message}
+            onConfrim={errorHandler}
+          />
+        )}
+        <div className="flex p-4 ">
+          <div className="mt-14 p-8   bg-gradient-to-r from-sky-900 via-sky-600 to-sky-950 border-x-gray-700 	items-center border-2 rounded-xl w-full lg:w-[35%]  shadow-lg mx-auto ">
+            <form onSubmit={userSubmissionHandler} className="">
+              <label
+                htmlFor="username"
+                className="block mt-2 w-full text-md font-bold leading-6 text-black"
+              >
+                User Name
+              </label>
+              <div className="flex rounded-md w-full  bg-white   shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="userName"
+                  value={userName}
+                  onChange={userNameHandler}
+                  className="block flex-1  border-0 bg-transparent py-1.5 pl-1 text-gray-800 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                />
+              </div>
 
-            <label
-              htmlFor="age"
-              className="  block mt-2  text-md font-bold leading-6 text-black"
-            >
-              Age ( Years )
-            </label>
-            <div className="flex rounded-md  bg-white   shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-              <input
-                type="number"
-                name="userAge"
-                value={userAge}
-                onChange={userAgeHandler}
-                className="block flex-1  border-0 bg-transparent py-1.5 pl-1 text-gray-800 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-              />
-            </div>
+              <label
+                htmlFor="age"
+                className="  block mt-2  text-md font-bold leading-6 text-black"
+              >
+                Age ( Years )
+              </label>
+              <div className="flex rounded-md  bg-white   shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="number"
+                  name="userAge"
+                  value={userAge}
+                  onChange={userAgeHandler}
+                  className="block flex-1  border-0 bg-transparent py-1.5 pl-1 text-gray-800 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                />
+              </div>
 
-            <button
-              type="submit"
-              className="mt-5 bg-slate-950 hover:bg-slate-800 shadow-md text-white font-bold py-2 px-4 rounded w-full sm:w-auto"
-            >
-              Add User
-            </button>
-          </form>
+              <button
+                type="submit"
+                className="mt-5 bg-slate-950 hover:bg-slate-800 shadow-md text-white font-bold py-2 px-4 rounded w-full sm:w-auto"
+              >
+                Add User
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
--- src/components/ErrorPopupModel.jsx
+++ src/components/ErrorPopupModel.jsx
@@ -2,18 +2,30 @@ import React from "react";
 
 const ErrorPopupModel = (props) => {
   return (
-    <div onClick={props.onConfrim} className="">
-      <div>
-        <header>
-          <h2>{props.title}</h2>
-        </header>
-        <div>
-          <p>{props.message}</p>
-        </div>
+    <div className=" fixed top-0 left-0 h-full w-full bg-black bg-opacity-60 ">
+      <div
+        onClick={props.onConfrim}
+        className="absolute flex w-full top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%]  "
+      >
+        <div className="border-white shadow-lg	items-center border-2 rounded-md mx-auto ">
+          <header className="py-2 px-12 rounded-md bg-black text-white">
+            <h2>{props.title}</h2>
+          </header>
+          <div className="bg-gradient-to-r from-[#bd97ff] to-[#cd97ff]">
+            <p className="py-2 px-12 text-black  bg-gradient-to-r from-[#bd97ff] to-[#cd97ff]">
+              {props.message}
+            </p>
 
-        <footer>
-          <button onClick={props.onConfirm}>Close</button>
-        </footer>
+            <footer className="flex justify-center pb-4">
+              <button
+                onClick={props.onConfirm}
+                className="mt-5  bg-slate-950 hover:bg-slate-800 shadow-md  text-white font-bold py-2 px-4 rounded w-auto"
+              >
+                Close
+              </button>
+            </footer>
+          </div>
+        </div>
       </div>
     </div>
   );
